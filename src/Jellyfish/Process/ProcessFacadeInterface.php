d39637f3e81810b25a46eb80db339e36c252ce4e
Daniel Rose <daniel-rose@gmx.de>
2021-03-01 14:50:22 +0100

    Add some improvements and provide facade only  (#40)
    * Provide facade only and some other improvements
    - use make instead of phing
    - update all dependencies
    - add dandelion stuff
    - use github actions instead of travis
    - add alpine compatibility (GLOB_BRACE is no supported by alpine)
    - remove deprecated matcher "at"
    - remove version check (PHP 8 compatibility)
    - use laminas packages instead of zend packages
    - ...

--- src/Jellyfish/Process/ProcessFacadeInterface.php
+++ src/Jellyfish/Process/ProcessFacadeInterface.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Jellyfish\Process;
 
-interface ProcessFactoryInterface
+interface ProcessFacadeInterface
 {
     /**
-     * @param array $command
+     * @param string[] $command
      *
      * @return \Jellyfish\Process\ProcessInterface
      */
-    public function create(array $command): ProcessInterface;
+    public function createProcess(array $command): ProcessInterface;
 }
